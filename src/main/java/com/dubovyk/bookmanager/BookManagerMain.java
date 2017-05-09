4599ff70408888f3000ec27131a8979e73341a5d
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2017-05-09 19:30:09 +0300

    Initial commit

---
+++ src/main/java/com/dubovyk/bookmanager/BookManagerMain.java
@@ -0,0 +1,11 @@
+package com.dubovyk.bookmanager;
+
+/**
+ * @author Sergey Dubovyk aka knidarkness
+ * @version 1.0
+ */
+public class BookManagerMain {
+    public static void main(String ... args){
+        System.out.println("Hello");
+    }
+}
